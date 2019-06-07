29e9198051c65058597ee1f44201fa2fe93e7619
gabolinno <gabolinno@gmail.com>
2019-06-07 15:15:39 +0300

    MVC folders

---
+++ Models/index_model.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * The home page model
+ */
+class IndexModel
+{
+
+    private $message = 'Welcome to Home page.';
+
+    function __construct()
+    { }
+
+    public function welcomeMessage()
+    {
+        $this->render(filename: "home.php");
+    }
+}
