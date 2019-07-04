80baf528a8e6a85579bb73a680f8c017d2356daa
Slavi Stoev <slavi.stoev97@gmail.com>
2019-07-04 18:50:38 +0300

    Updated registration

--- app/controllers/register.php
+++ app/controllers/register.php
@@ -16,14 +16,15 @@ class Register extends Controller {
             $username_regex = '/[\w]{6,25}/i';
             $password_regex = '~(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])^[\w]{6,30}$~';
 
+            $error = '';
             if (!preg_match($username_regex, $username)) {
-                echo "Username must be between 6 and 25 characters(letters, digits and underscore).";
+                $error = "Username must be between 6 and 25 characters(letters, digits and underscore).";
             }
             else if (!preg_match($password_regex, $password)) {
-                echo "Password must be between 6 and 30 characters, containing at least one lower case letter, one upper case letter and one digit";
+                $error = "Password must be between 6 and 30 characters, containing at least one lower case letter, one upper case letter and one digit";
             }
             else if (strcmp($password, $repeat_password)) {
-                echo "Password and repeat password must match";
+                $error = "Password and repeat password must match";
             }
             else {
                 $user = new User($username, $password, $email);
@@ -31,9 +32,10 @@ class Register extends Controller {
                 if ($result['success']) {
                     header("Location: ./login");
                 } else {
-                    echo $result['error'];
+                    $error = $result['error'];
                 }
             }
+            echo $error;
         }
 
     }
