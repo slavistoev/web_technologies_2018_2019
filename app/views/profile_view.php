b83ab15dd0a85b263230073dea4d71bd2dbddb6c
gabolinno <gabolinno@gmail.com>
2019-07-04 18:37:00 +0300

    Profile added

--- app/views/profile_view.php
+++ app/views/profile_view.php
@@ -32,12 +32,52 @@
         ?>
     </nav>
     <article>
-        <div class="container">
-            <div>
-                
+        <div class="tabContainer">
+            <div class="buttonContainer">
+                <button onclick="showPanel(0)" class="active" >My profile:</button>
+                <button onclick="showPanel(1)">My pixels:</button>
+                <button onclick="showPanel(2)">Settings:</button>
             </div>
+            <div class="tabPanel active">
+                Tab 1: Content
+            </div>  
+            <div class="tabPanel">
+                Tab 2: Content
+            </div>  
+            <div class="tabPanel">
+                Tab 3: Content
+            </div>  
+            <div class="tab">
+                Welcome To Your Profile!
+            </div> 
         </div>
     </article>
 </body>
+
+<script language="javascript" type="text/javascript">
+    var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
+    var tabPanels=document.querySelectorAll(".tabContainer .tabPanel");
+
+    tabPanels[0].style.display="block";
+    tabButtons[0].style.backgroundColor="#99989869";
+    tabButtons[0].style.color="white";
+
+    function showPanel(panelIndex) {
+        tabButtons.forEach(function(node) {
+            node.style.backgroundColor="";
+            node.style.color="";
+        });
+        tabButtons[panelIndex].style.backgroundColor="#99989869";
+        tabButtons[panelIndex].style.color="white";
+
+        tabPanels.forEach(function(node) {
+            node.style.display="none";
+        })
+        tabPanels[panelIndex].style.display="block";
+        
+        tabPanels[panelIndex].style.backgroundColor="#99989869";
+
+    }
+</script>
 
 </html>
